f97bd280147bd4f9f5c3fca06d74f0386a626f84
sto <Son.C.To@gmail.com>
2015-07-24 16:11:41 +0200

    rename

--- src/twitter/core.cljs
+++ src/twitter/core.cljs
@@ -32,29 +32,29 @@
                                    :node/components    [{:component/type :DOMElement
                                                          :fontSize       "30px"
                                                          :lineHeight     "100px"
-                                                         :classes ["header"]
+                                                         :classes        ["header"]
                                                          :content        "Twitter"}]}
 
                                   {:node/id                "swapper"
                                    :node/differential-size [nil -200 nil]
                                    :node/position          [0 100]
-                                   :node/components [{:component/type :DOMElement}]
+                                   :node/components        [{:component/type :DOMElement}]
                                    :node/children          (for [{:keys [id]} sections]
                                                              {:node/id         (str "section-" id)
                                                               :node/components [{:component/type :DOMElement
                                                                                  :overflow-y     "scroll"
                                                                                  :overflow-x     "hidden"}]
                                                               :node/children   (for [i (range 25)]
-                                                                                 {:node/size-mode       [DEFAULT ABSOLUTE]
-                                                                                  :node/absolute-size   [nil 100]
-                                                                                  :node/position        [0 (* 100 i)]
-                                                                                  :node/components [{:component/type :DOMElement
-                                                                                                     :backgroundColor (str "#" (.toString (rand-int 16rFFFFFF) 16))
-                                                                                                     :boxingSize "border-box"
-                                                                                                     :lineHeight "100px"
-                                                                                                     :borderBottom "1px solid black"
-                                                                                                     :font-size "12px"
-                                                                                                     :content (str "tweet" i)}]
+                                                                                 {:node/size-mode     [DEFAULT ABSOLUTE]
+                                                                                  :node/absolute-size [nil 100]
+                                                                                  :node/position      [0 (* 100 i)]
+                                                                                  :node/components    [{:component/type  :DOMElement
+                                                                                                        :backgroundColor (str "#" (.toString (rand-int 16rFFFFFF) 16))
+                                                                                                        :boxingSize      "border-box"
+                                                                                                        :lineHeight      "100px"
+                                                                                                        :borderBottom    "1px solid black"
+                                                                                                        :font-size       "12px"
+                                                                                                        :content         (str "tweet" i)}]
                                                                                   })})}
                                   {:node/id            "footer"
                                    :node/size-mode     [DEFAULT ABSOLUTE]
@@ -69,25 +69,25 @@
                                                            {:node/id                (str "footer-" id)
                                                             :node/align             [(/ i num-sections)]
                                                             :node/proportional-size [(/ 1 num-sections)]
-                                                            :node/components        [{:component/type  :DOMElement
-                                                                                      :textAlign       "center"
-                                                                                      :lineHeight      "100px"
-                                                                                      :fontSize        "18px"
-                                                                                      :cursor          "pointer"
-                                                                                      :classes         ["navigation"]
-                                                                                      :content         id}]}))}]})
+                                                            :node/components        [{:component/type :DOMElement
+                                                                                      :textAlign      "center"
+                                                                                      :lineHeight     "100px"
+                                                                                      :fontSize       "18px"
+                                                                                      :cursor         "pointer"
+                                                                                      :classes        ["navigation"]
+                                                                                      :content        id}]}))}]})
 
 (util/save scene-graph)
 
 (render-scene-graph "twitterus")
 
-(def channels (for [section-node (:node/children (get-node-by-id "footer"))]
-                (events->chan section-node "tap" (fn [] section-node))))
+(def channels (for [section-button-node (:node/children (get-node-by-id "footer"))]
+                (events->chan section-button-node "tap" (fn [] section-button-node))))
 
 (go
   (while true
-    (let [[section-node channel] (alts! channels)
-          famous-node (:node/famous-node section-node)
+    (let [[section-button-node channel] (alts! channels)
+          famous-node (:node/famous-node section-button-node)
           components (.. famous-node getComponents)]
       (doseq [c components]
         (if (= "DOMElement" (.. c -constructor -name))
