cc87843058c036f538a000e6b851bad6d08aa44b
sto <Son.C.To@gmail.com>
2015-07-31 15:28:59 +0100

    form

--- src/twitter/core.cljs
+++ src/twitter/core.cljs
@@ -6,8 +6,10 @@
 (enable-console-print!)
 
 (defonce Size (.. famous -components -Size))
+(defonce NODE (.. famous -core -Node))
 (defonce ABSOLUTE (.. Size -ABSOLUTE))
 (defonce DEFAULT (.. Size -RELATIVE))
+(defonce RENDER_SIZE (.. NODE -RENDER_SIZE))
 
 (def scene-graph {:node/id       "twitterus"
                   :node/children [{:node/id            "header"
@@ -18,60 +20,67 @@
                                                          :lineHeight     "100px"
                                                          :classes        ["header"]
                                                          :content        "Authentication"}]}
-                                  {:node/id          "login-form"
-                                   :node/align       [0 0.1]
-                                   :node/components  [{:component/type :DOMElement
-                                                       :overflow-y     "hidden"
-                                                       :overflow-x     "hidden"}
-                                                      {:component/type :Align}]
-                                   :node/children    [{:node/size-mode     [DEFAULT ABSOLUTE]
-                                                       :node/absolute-size [nil 100]
-                                                       :node/position      [0 100]
-                                                       :node/components    [{:component/type  :DOMElement
-                                                                             :backgroundColor (str "#" (.toString (rand-int 16rFFFFFF) 16))
-                                                                             :boxingSize      "border-box"
-                                                                             :lineHeight      "100px"
-                                                                             :borderBottom    "1px solid black"
-                                                                             :font-size       "12px"
-                                                                             :content         (str "Login")}]
-                                                       }]}
+                                  {:node/id         "login-form"
+                                   :node/align      [0 0.1]
+                                   :node/components [{:component/type :DOMElement
+                                                      :overflow-y     "hidden"
+                                                      :overflow-x     "hidden"}
+                                                     {:component/type :Align}]
+                                   :node/children   [{:node/size-mode   [RENDER_SIZE RENDER_SIZE]
+                                                      :node/align       [0.5 0.1]
+                                                      :node/mount-point [1 0]
+                                                      :node/components  [{:component/type :DOMElement
+                                                                          :boxingSize     "border-box"
+                                                                          :borderBottom   "1px solid black"
+                                                                          :font-size      "20px"
+                                                                          :content        "Email"}]
+                                                      }
+
+                                                     {:node/size-mode   [RENDER_SIZE RENDER_SIZE]
+                                                      :node/align       [0.5 0.1]
+                                                      :node/mount-point [0 0]
+                                                      :node/components  [{:component/type :DOMElement
+                                                                          :borderBottom   "1px solid black"
+                                                                          :font-size      "12px"
+                                                                          :content        "<input id='email' type='text' />"}]
+                                                      }
+
+
+                                                     {:node/size-mode   [RENDER_SIZE RENDER_SIZE]
+                                                      :node/align       [0.5 .15]
+                                                      :node/mount-point [1 0]
+                                                      :node/components  [{:component/type :DOMElement
+                                                                          :borderBottom   "1px solid black"
+                                                                          :font-size      "20px"
+                                                                          :content        "Password"}]
+                                                      }
+                                                     {:node/size-mode   [RENDER_SIZE RENDER_SIZE]
+                                                      :node/align       [0.5 .15]
+                                                      :node/mount-point [0 0]
+                                                      :node/components  [{:component/type :DOMElement
+                                                                          :boxingSize     "border-box"
+                                                                          :borderBottom   "1px solid black"
+                                                                          :content        "<input id='password' type='password' />"}]
+                                                      }
+
+                                                     {:node/id            "login-button"
+                                                      :node/size-mode     [ABSOLUTE RENDER_SIZE]
+                                                      :node/absolute-size [60]
+                                                      :node/align         [0.5 .18]
+                                                      :node/mount-point   [0 0]
+                                                      :node/components    [{:component/type :DOMElement
+                                                                            :content        "<button>Log in </button>"}]
+                                                      }
+                                                     ]}
                                   ]})
 
+(infamous/render-scene-graph scene-graph "body")
 
+(def login-clicks (events->chan (infamous/get-node-by-id "login-button") "tap" #(identity "login")))
 
-(defn get-dom-element-and-align-component [section-name]
-  (let [footer-section-node (infamous/get-node-by-id (str "footer-" section-name))
-        section-node (infamous/get-node-by-id (str "section-" section-name))
-        dom-element (infamous/get-famous-component-by-type-name footer-section-node "DOMElement")
-        align-component (infamous/get-famous-component-by-type-name section-node "Align")]
-    [dom-element align-component]))
-
-(defonce TRANSITION-DURATION 500)
-
-(defn show [section-name]
-  (let [[section-dom-element section-align] (get-dom-element-and-align-component section-name)
-        header-node (infamous/get-node-by-id "header")
-        header-dom-element (infamous/get-famous-component-by-type-name header-node "DOMElement")]
-    (.. header-dom-element (setContent section-name))
-    (.. section-dom-element (removeClass "off") (addClass "on"))
-    (.. section-align (set 0 0 0 (clj->js {:duration TRANSITION-DURATION})))))
-
-(defn hide [section-name]
-  (let [[dom-element align-component] (get-dom-element-and-align-component section-name)]
-    (.. dom-element (removeClass "on") (addClass "off"))
-    (.. align-component (set 1 0 0 (clj->js {:duration TRANSITION-DURATION})))))
-
-(defn hide-all-sections-except [section-name]
-  (let [selected-footer-button-node (infamous/get-node-by-id (str "footer-" section-name))
-        footer-button-nodes (:node/children (infamous/get-node-by-id "footer"))
-        unselected-nodes (filter #(not= % selected-footer-button-node) footer-button-nodes)
-        unselected-section-names (map #(:twitter/section-name %) unselected-nodes)]
-    (doseq [section-name unselected-section-names]
-      (hide section-name))))
-
-(defn switch-on [section-name]
-  (show section-name)
-  (hide-all-sections-except section-name))
-
-(infamous/render-scene-graph scene-graph "body")            ;render the scene-graph and mount it on the "body" css selector
-
+(go (while true
+      (println (<! login-clicks))
+      (let [email (.. js/document (getElementById "email") -value)
+            password (.. js/document (getElementById "password") -value)]
+        (println email " password=" password))
+      ))
